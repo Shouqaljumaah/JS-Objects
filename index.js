d458e7064a48366bcf96c7468cfb1f5b15ffa112
SHOUQ ALJUMAH <shouqaljumaah@gmail.com>
2024-11-05 19:06:47 +0300

    task

---
+++ index.js
@@ -0,0 +1,24 @@
+const book = {
+  Title: "JavaScript: The Definitive Guide",
+  Author: "David Flanagan",
+  PublishedYear: 2020,
+  Genre: "Programming",
+};
+console.log(book.Title, book.PublishedYear);
+
+book.pageCount = 1096;
+console.log(book);
+
+book.ISBN = "978 - 1491952023";
+console.log(book);
+
+book.PublishedYear = 2021;
+console.log(book);
+
+book.Author = ["David Flanagan", "Another Author"];
+console.log(book);
+
+book.reviews = [
+  { reviewer: "Book Critic", comment: "A comprehensive guide to JavaScript." },
+];
+console.log(book);
